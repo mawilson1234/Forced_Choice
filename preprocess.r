5d56286a93d2d228bfb9c8a054776329681d4c15
mawilson1234 <michael.a.wilson@yale.edu>
2025-05-02 15:40:47 -0400

    preprocess

--- preprocess.r
+++ preprocess.r
@@ -48,14 +48,14 @@ read.pcibex <- function(
 			cols <- get.colnames(colnames.from)
 		}
 		
-		return (read.csv(filepath, comment.char = '#', header = FALSE, col.names = cols))
+		return (read.csv(filepath, comment.char = '#', header = FALSE, col.names = cols, fileEncoding = 'UTF-8'))
 	}
 	else {
-		return (read.csv(filepath, comment.char = '#', header = FALSE, col.names = seq_len(n.cols)))
+		return (read.csv(filepath, comment.char = '#', header = FALSE, col.names = seq_len(n.cols), fileEncoding = 'UTF-8'))
 	}
 }
 
-df <- read.pcibex('results.csv') |>
+df <- read.pcibex('results_prod.csv') |>
 	as_tibble() |>
 	mutate(
 		participant = paste0(Results.reception.time, MD5.hash.of.participant.s.IP.address),
@@ -64,38 +64,56 @@ df <- read.pcibex('results.csv') |>
 	select(-Results.reception.time, -MD5.hash.of.participant.s.IP.address) |>
 	select(participant, everything())
 
-demographics <- df |>
-	filter(grepl('^demographics', Parameter)) |>
-	select(participant, Parameter, Value) |>
-	mutate(
-		Parameter = gsub('^demographics_', '', Parameter),
-		Value = Value |> trimws(),
-		Value = gsub('%2C', ',', Value)
-	) |>
-	pivot_wider(
-		names_from = Parameter,
-		values_from = Value
-	)
-
-demographics |>
-	fwrite('demographics.csv', row.names = FALSE)
-
 df <- df |>
-	filter(PennElementType == 'Selector') |>
+	filter(PennElementName == 'mc_question') |>
 	select(
-		participant, group:third_answer_type,
-		Value
+		participant, group,
+		Parameter, Value, item:distractor_question,
+		correct_answers:response_time_mc
 	) |>
 	rename(response = Value) |>
 	mutate(
-		sentence = gsub('%2C', ',', sentence),
-		condition = condition |> trimws()
+		item = as.numeric(item),
+		response = case_when(
+			response == 'checked' ~ Parameter |> 
+				str_replace_all('mc_answer_(.*?)$', '\\1') |>
+				trimws(),
+			TRUE ~ NA_character_
+		),
+		distractor_question = distractor_question |>
+			trimws(),
+		sentence = sentence |>
+			str_replace_all('%2C', ',') |>
+			trimws(),
+		correct_answers = correct_answers |>
+			trimws() |>
+			str_replace_all('%2C', ',') |>
+			str_replace_all(', ', ',')
 	) |>
+	select(-Parameter) |>
+	group_by(
+		participant, group, item, sentence, condition, 
+		distractor_question, correct_answers, reading_time_sentence,
+		response_time_distractor, response_time_mc
+	) |>
+	summarize(response = paste0(response[!is.na(response)], collapse = ',')) |>
+	ungroup() |>
+	mutate(
+		response_accuracy = mapply(
+			\(ca, r) {
+				as.numeric(
+					all(sort(str_split_1(ca, ',')) == sort(str_split_1(r, ',')))
+				)
+			},
+			correct_answers, response
+		)
+	) |> 
 	select(
-		participant, group, item, sentence, image, condition,
-		question, response, response_time, first_answer:third_answer_type
-	) |>
-	arrange(participant, item)
+		participant, group, item, condition, sentence, 
+		reading_time_sentence, distractor_question, 
+		response_time_distractor, correct_answers, 
+		response, response_accuracy, response_time_mc
+	)
 
 df |>
-	fwrite('cleaned_results.csv', row.names = FALSE)
+	write.csv('cleaned_results.csv', row.names = FALSE)
